5b7f70e538175678f1065354cb5f8dd23fa6f1ba
nixiesquid <audu817@gmail.com>
2020-03-13 02:20:43 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate gtk;
+extern crate gio;
+
+use gtk::{ WidgetExt, WindowExt };
+use gio::{ ApplicationExt };
+
+fn main() {
+    match gtk::Application::new("com.github.nixiesquid.note_bit", gio::APPLICATION_HANDLES_OPEN) {
+        Ok(app) => {
+            app.connect_activate(|app| {
+                let win = gtk::ApplicationWindow::new(&app);
+                win.set_title("Hello Gtk-rs");
+                win.show_all();
+            });
+
+            app.run(&[""]);
+        },
+        Err(_) => {
+            println!("Application start up error");
+        }
+};
